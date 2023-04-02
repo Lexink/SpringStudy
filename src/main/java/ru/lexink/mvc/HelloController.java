be063cc03f3014cb0c143d021b28943e52293362
lexink <lexink@mail.ru>
2023-04-02 13:22:32 +0300

    Spring 20

--- src/main/java/ru/lexink/mvc/HelloController.java
+++ src/main/java/ru/lexink/mvc/HelloController.java
@@ -1,13 +1,23 @@
 package ru.lexink.mvc;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "hello";
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "surname", required = false) String surname,
+                           Model model){
+
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+        return "first/hello";
     }
 }
