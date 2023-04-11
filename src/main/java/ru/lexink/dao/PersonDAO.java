5409269db840123b211745438eacd36f4e616da3
lexink <lexink@mail.ru>
2023-04-12 00:08:56 +0300

    Spring 25

--- src/main/java/ru/lexink/dao/PersonDAO.java
+++ src/main/java/ru/lexink/dao/PersonDAO.java
@@ -2,6 +2,8 @@ package ru.lexink.dao;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
 import java.util.concurrent.ThreadLocalRandom;
 import ru.lexink.entity.Person;
 
@@ -11,49 +13,85 @@ import java.util.List;
 //@Repository
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "admin";
+    private static Connection connection;
 
-    {
-        int limit = PEOPLE_COUNT + 10;
-        people = new ArrayList<>();
-        for (int i = PEOPLE_COUNT; i < limit; i++, PEOPLE_COUNT++) {
-            Person person = new Person();
-            person.setId(i);
-            person.setName("User_" + i);
-            person.setAge(random(20, 50));
-            person.setEmail(String.format("%d%d%d@xxxx.com", i, i, i));
-            people.add(person);
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
+
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try{
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
-    public Person show(int id){
-
-       return people.stream()
-                .filter(person -> person.getId() == id)
-                .findAny()
-                .orElseThrow();
-    }
+   public Person show(int id){
+//
+//       return people.stream()
+//                .filter(person -> person.getId() == id)
+//                .findAny()
+//                .orElseThrow();
+       return null;
+   }
 
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try{
+            String SQL = "INSERT INTO person VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.setInt(1,7);
+            statement.setString(2, person.getName());
+            statement.setInt(3, person.getAge());
+            statement.setString(4, person.getEmail());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void update(int id, Person person){
-        Person personToUpdate = show(id);
-        personToUpdate.setName(person.getName());
-        personToUpdate.setEmail(person.getEmail());
-        personToUpdate.setSurname(person.getSurname());
-        personToUpdate.setAge(person.getAge());
+//        Person personToUpdate = show(id);
+//        personToUpdate.setName(person.getName());
+//        personToUpdate.setEmail(person.getEmail());
+//        personToUpdate.setSurname(person.getSurname());
+//        personToUpdate.setAge(person.getAge());
     }
 
     public void delete(int id){
-        people.removeIf(p -> p.getId() == id);
+
+//        people.removeIf(p -> p.getId() == id);
     }
 
     private int random(int min, int max) {
