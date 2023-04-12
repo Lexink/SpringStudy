333ca3d4b5471c54dae044af20e99bc68a8b3fc3
lexink <lexink@mail.ru>
2023-04-12 23:12:52 +0300

    Spring 28 1. Конфигурация БД из внешнего файла

--- src/main/java/ru/lexink/config/MySpringWebConfig.java
+++ src/main/java/ru/lexink/config/MySpringWebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,15 +17,19 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 import java.sql.DriverManager;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan("ru.lexink")
 @EnableWebMvc
+@PropertySource("classpath:db.properties")
 public class MySpringWebConfig implements WebMvcConfigurer {
     private final ApplicationContext context;
+    private final Environment environment;
 
-    public MySpringWebConfig(ApplicationContext context) {
+    public MySpringWebConfig(ApplicationContext context, Environment environment) {
         this.context = context;
+        this.environment = environment;
     }
 
     @Bean
@@ -53,10 +59,10 @@ public class MySpringWebConfig implements WebMvcConfigurer {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("admin");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
 
         return dataSource;
     }
